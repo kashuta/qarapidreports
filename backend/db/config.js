c475da742e2bf5f40899b717e929c48e2413a6a3
Artem Kashuta <zoich@MacBook-Pro-Artem.local>
2023-04-02 12:39:08 +0300

    small fix

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,25 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-    },
-    test: {
-        username: process.env.DB_TEST_USER,
-        password: process.env.DB_TEST_PASS,
-        database: process.env.DB_TEST_NAME,
-        host: process.env.DB_TEST_HOST,
-        dialect: process.env.DB_TEST_DIALECT,
-    },
-    production: {
-        username: process.env.DB_PROD_USER,
-        password: process.env.DB_PROD_PASS,
-        database: process.env.DB_PROD_NAME,
-        host: process.env.DB_PROD_HOST,
-        dialect: process.env.DB_PROD_DIALECT,
-    }
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASS,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_TEST_HOST,
+    dialect: process.env.DB_TEST_DIALECT,
+  },
+  production: {
+    username: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASS,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_PROD_HOST,
+    dialect: process.env.DB_PROD_DIALECT,
+  },
+};
--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,32 @@
-require("dotenv").config();
+const dotenv = require('dotenv');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const morgan = require("morgan");
-const { sequelize } = require("./db/models");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const morgan = require('morgan');
+const { sequelize } = require('./db/models');
 
 const app = express();
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+
 dotenv.config({ path: envFile });
-//Middlewares
-app.use(morgan("dev"));
+// Middlewares
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: true,
-    credentials: true
+  origin: true,
+  credentials: true,
 }));
 app.use(express.json());
-app(cookieParser());
 
-//Routes
+// Routes
 app.use('/api', require('./routes'));
 
-//Error handling
-app.use((err, req, res, next) => {
-    res.status(422).send({ error: err.message });
-});
+// Server
 
-//Server
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
-
