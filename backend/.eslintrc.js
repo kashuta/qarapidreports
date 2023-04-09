36f6238052b2b206a48b4a153c585fb4553fe88b
Artem Kashuta <a@artka.dev>
2023-04-09 08:04:00 +0300

    linter fix

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
     node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
+    'eslint:recommended',
+    'airbnb-base',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: [
-    'react',
-  ],
   rules: {
+    // Add custom rules here
   },
 };
