9e7dd6edead123860f988c113c7da490d14971f6
Akhmed <nob_32@mail.ru>
2023-04-12 14:24:31 +0300

    mmm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import Container from '@mui/material/Container';
 import { Route, Routes } from 'react-router-dom';
 import SignInForm from './components/Auth/SignInForm';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
 import InspectorMain from './components/InspectorMain/InspectorMain';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import FormGME0024 from './components/Forms/FormGME0024';
 
 function App() {
   return (
-    <Container>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Navbar />
       <Routes>
         <Route path="/inspector" element={<InspectorMain />} />
         <Route path="/signin" element={<SignInForm />} />
+        <Route path="/FormGME0024" element={<FormGME0024 />} />
       </Routes>
-    </Container>
+    </LocalizationProvider>
   );
 }
 
