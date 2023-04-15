ffd2177830a56d1681d2880dc98058c129034132
Svyatoslav <svyatoslav.gryadyuga@gmail.com>
2023-04-15 17:01:02 +0400

    fixes

--- frontend/src/components/Auth/SignInForm.jsx
+++ frontend/src/components/Auth/SignInForm.jsx
@@ -20,7 +20,7 @@ function SignInForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    fetch('http://localhost:3001/api/v2/login', {
+    fetch('http://localhost:3001/api/v2/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
