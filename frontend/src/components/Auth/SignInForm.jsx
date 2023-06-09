509a36e5ebd900b7ba0c8763a8f08d3381452098
Konstantin <117216684+shevchenkostya@users.noreply.github.com>
2023-06-09 13:42:55 +0500

    footer bug fixed

--- frontend/src/components/Auth/SignInForm.jsx
+++ frontend/src/components/Auth/SignInForm.jsx
@@ -122,3 +122,132 @@ function SignInForm() {
 }
 
 export default SignInForm;
+
+// import React, { useState } from 'react';
+// import { Field, Form, Formik } from 'formik';
+// import * as Yup from 'yup';
+// import {
+//   Button, IconButton, InputAdornment, TextField,
+// } from '@mui/material';
+// import { Box } from '@mui/system';
+// import Visibility from '@mui/icons-material/Visibility';
+// import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+// import { useDispatch } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+
+// import { getUserLoaderAction, setUserAction } from '../../Redux/user.action';
+
+// const validationSchema = Yup.object({
+//   email: Yup.string().email('Invalid email format').required('Required'),
+//   password: Yup.string().min(8, 'Password must be at least 8 characters').required('Required'),
+// });
+
+// function SignInForm() {
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const handleClickShowPassword = () => setShowPassword((show) => !show);
+//   const handleMouseDownPassword = (event) => event.preventDefault();
+
+//   return (
+//     <Formik
+//       initialValues={{
+//         email: '',
+//         password: '',
+//       }}
+//       validationSchema={validationSchema}
+//       onSubmit={(values, { setSubmitting }) => {
+//         dispatch(getUserLoaderAction(false));
+
+//         fetch('http://localhost:3001/api/v2/auth/login', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify(values),
+//           credentials: 'include',
+//         })
+//           .then((res) => res.json())
+//           .then((data) => {
+//             if (data.message) {
+//               alert(data.message);
+//               dispatch(getUserLoaderAction(true));
+//               navigate('/login');
+//             } else {
+//               const token = data.accessToken;
+//               localStorage.setItem('accessToken', token);
+//               dispatch(setUserAction(data.userFront));
+//               dispatch(getUserLoaderAction(true));
+//               navigate('/');
+//             }
+//           })
+//           .catch(console.log)
+//           .finally(() => setSubmitting(false));
+//       }}
+//     >
+//       {({ isSubmitting }) => (
+//         <Form>
+//           <Box
+//             container="true"
+//             display="flex"
+//             alignItems="center"
+//             justify="center"
+//             flexDirection="column"
+//             margin="100px 400px"
+//           >
+//             <Field
+//               component={TextField}
+//               required
+//               label="Email"
+//               name="email"
+//               variant="outlined"
+//               sx={{
+//                 marginBottom: '1rem',
+//                 width: '100%',
+//               }}
+//             />
+//             <Field
+//               component={TextField}
+//               required
+//               label="Password"
+//               name="password"
+//               type={showPassword ? 'text' : 'password'}
+//               variant="outlined"
+//               sx={{
+//                 marginBottom: '1rem',
+//                 width: '100%',
+//               }}
+//               InputProps={{
+//                 endAdornment: (
+//                   <InputAdornment position="end">
+//                     <IconButton
+//                       aria-label="toggle password visibility"
+//                       onClick={handleClickShowPassword}
+//                       onMouseDown={handleMouseDownPassword}
+//                     >
+//                       {showPassword ? <VisibilityOff /> : <Visibility />}
+//                     </IconButton>
+//                   </InputAdornment>
+//                 ),
+//               }}
+//             />
+//             <Button
+//               variant="contained"
+//               type="submit"
+//               disabled={isSubmitting}
+//               sx={{
+//                 width: '100%',
+//               }}
+//             >
+//               Login
+//             </Button>
+//           </Box>
+//         </Form>
+//       )}
+//     </Formik>
+//   );
+// }
+
+// export default SignInForm;
